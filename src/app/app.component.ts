5c0a1992dae7b2893ff92b2efda81b0baa734e26
JasonCrk <emerzonkolki@gmail.com>
2024-04-21 12:50:38 -0500

    feat(app.component.ts): Remove title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router'
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: `<h1>Hola Grupo</h1>
-    <router-outlet />`,
+  template: `<router-outlet />`,
 })
 export class AppComponent {}
+
