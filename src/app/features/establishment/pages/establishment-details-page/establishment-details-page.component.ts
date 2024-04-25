05c4c39594fab1db3322b2f692e5a4b72369f629
JasonCrk <emerzonkolki@gmail.com>
2024-04-25 15:03:39 -0500

    feat(establishment-details-page): Remove inline styles

--- src/app/features/establishment/pages/establishment-details-page/establishment-details-page.component.ts
+++ src/app/features/establishment/pages/establishment-details-page/establishment-details-page.component.ts
@@ -5,7 +5,6 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './establishment-details-page.component.html',
-  styles: ``
 })
 export class EstablishmentDetailsPageComponent {
 
