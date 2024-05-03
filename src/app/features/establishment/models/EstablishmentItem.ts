05481e4262f11da242308079b245542c0347114d
JasonCrk <emerzonkolki@gmail.com>
2024-05-03 16:40:50 -0500

    feat: establishment details page

--- src/app/features/establishment/models/EstablishmentItem.ts
+++ src/app/features/establishment/models/EstablishmentItem.ts
@@ -1,9 +1,6 @@
-import { TopicItem } from '../../topic/models/TopicItem'
-
 export interface EstablishmentItem {
   id: string
   name: string
   maxCapacity: number
-  topics: TopicItem[]
   firstImage: string
 }
--- src/app/features/establishment/services/api-establishment.service.ts
+++ src/app/features/establishment/services/api-establishment.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
 
 import type { EstablishmentItem } from '../models/EstablishmentItem';
+import type { EstablishmentDetails } from '../models/EstablishmentDetails';
 import type { SearchAvailableEstablishmentsParams } from '../models/SearchAvailableEstablishmentsParams';
 
 @Injectable({
@@ -14,6 +15,10 @@ export class ApiEstablishmentService {
   private readonly _http = inject(HttpClient)
 
   private readonly BASE_API_URL = `${environment.baseApiUrl}/establishments`
+
+  getOne(establishmentId: string) {
+    return this._http.get<EstablishmentDetails>(`${this.BASE_API_URL}/${establishmentId}`)
+  }
 
   searchAvailable(params?: SearchAvailableEstablishmentsParams) {
     return this._http.get<EstablishmentItem[]>(this.BASE_API_URL + '/search/available', params !== undefined
