70d95a6726cc801532d8f04fbb98f0a23945683b
Emerzon Javier Kolki Martinez <73272783+JasonCrk@users.noreply.github.com>
2024-05-08 18:36:54 -0500

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   baseApiUrl: 'http://localhost:4000/api/v1',
-  stripePublishableKey: 'pk_test_51P0rK8P1pDrg5jZEmI5R8R6hMBWt8q75PbyzlKBoYSqCDeEmWkd5YoQ0w9FZBhLYXx8UrjWzW2Tg73ZyAMvuIBJm00VdKCs3kI'
+  stripePublishableKey: ''
 };
